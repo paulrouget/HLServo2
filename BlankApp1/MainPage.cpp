cd2b32a650bc99b4bf0725a4677bef39835a42cd
Paul Rouget <me@paulrouget.com>
2019-07-09 09:06:08 +0200

    Add project files.

---
+++ BlankApp1/MainPage.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "logs.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+#include "AppView.h"
+
+// FIXME: necessary for ApplicationView::GetForCurrentView() to work.
+// https://devblogs.microsoft.com/oldnewthing/20190530-00/?p=102529
+#include "winrt/Windows.UI.ViewManagement.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::ViewManagement;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Holographic;
+
+namespace winrt::BlankApp1::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        if (HolographicSpace::IsAvailable())
+        {
+          log("Holographic space available");
+          auto v = winrt::Windows::ApplicationModel::Core::CoreApplication::CreateNewView(Foobar::AppViewSource());
+            int parentId = ApplicationView::GetForCurrentView().Id();
+            v.Dispatcher().RunAsync(Windows::UI::Core::CoreDispatcherPriority::Low, [parentId]() {
+            int winId = ApplicationView::GetForCurrentView().Id();
+            ApplicationViewSwitcher::TryShowAsStandaloneAsync(winId, ViewSizePreference::Default, parentId, ViewSizePreference::Default);
+            log("Immersive view started");
+          });
+        }
+        else {
+          log("Holographic space not available");
+        }
+    }
+}
---
+++ BlankApp1/pch.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+
+#include <future>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Navigation.h>
+
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Metadata.h>
+#include <winrt/Windows.Gaming.Input.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.Graphics.Holographic.h>
+#include <winrt/Windows.Perception.People.h>
+#include <winrt/Windows.Perception.Spatial.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Input.Spatial.h>
